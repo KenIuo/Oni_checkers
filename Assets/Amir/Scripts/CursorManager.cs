8c7fe9bf644517a78ba0792e3dde4f71e4eb7e74
Adrian32 <amir-rahim@mail.ru>
2023-11-25 20:19:38 +0300

    1.0.5 (25.11/3)
    Изменения:
    - изменено разрешение курсоров

---
+++ Assets/Amir/Scripts/CursorManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    [SerializeField] Texture2D cursor1;
+    [SerializeField] Texture2D cursor2;
+
+    Vector2 _cursorHotspot = new Vector2(40, 5);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

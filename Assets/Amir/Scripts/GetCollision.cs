e435870a9430d24754b4fa145bce203d8a2a7454
Adrian32 <amir-rahim@mail.ru>
2023-12-01 23:44:29 +0300

    1.1.0 (01.12/2) мелкие изменения

--- Assets/Amir/Scripts/GetCollision.cs
+++ Assets/Amir/Scripts/GetCollision.cs
@@ -2,14 +2,9 @@ using UnityEngine;
 
 public class GetCollision : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerExit(Collider other)
     {
-        GameManager.Instance.SoundManager.PlayDeathSound();
-        // воспроизводить анимацию смерти шашки
-        other.GetComponentInParent<CheckerController>().SetState(CheckerState.Died);
-
-        //TurnSystem.Instance.CheckEndOfGameConditions(other.gameObject.GetComponent<CheckerController>());
-        //TurnSystem.Instance.GetDeathByFall().OnEnter(other.gameObject);
-        //FindFirstObjectByType<DeathByFall>().OnEnter(other.gameObject);
+            GameManager.Instance.SoundManager.PlayDeathSound();
+            other.GetComponentInParent<CheckerController>().SetState(CheckerState.Died);
     }
 }

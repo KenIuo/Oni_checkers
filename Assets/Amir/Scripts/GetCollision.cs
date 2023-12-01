5f99cee8bbe5188e61578774d030feac30249509
Adrian32 <amir-rahim@mail.ru>
2023-12-01 22:51:30 +0300

    1.1.0 (01.12/1)
    небольшие изменения префабов

--- Assets/Amir/Scripts/GetCollision.cs
+++ Assets/Amir/Scripts/GetCollision.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class GetCollision : MonoBehaviour
 {
-    private void OnTriggerExit(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         GameManager.Instance.SoundManager.PlayDeathSound();
+        // воспроизводить анимацию смерти шашки
         other.GetComponentInParent<CheckerController>().SetState(CheckerState.Died);
+
+        //TurnSystem.Instance.CheckEndOfGameConditions(other.gameObject.GetComponent<CheckerController>());
+        //TurnSystem.Instance.GetDeathByFall().OnEnter(other.gameObject);
+        //FindFirstObjectByType<DeathByFall>().OnEnter(other.gameObject);
     }
 }
--- Assets/Amir/Scripts/RemoveFromCamera.cs
+++ Assets/Amir/Scripts/RemoveFromCamera.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class RemoveFromCamera : MonoBehaviour
 {
-    //[SerializeField] LayerMask _UIHideableLayer;
+    [SerializeField] LayerMask _UIHideableLayer;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == LayersTags.HIDER_LAYER)
+        if (other.gameObject.layer == _UIHideableLayer)
             other.gameObject.transform.GetChild(0).gameObject.SetActive(false);
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.layer == LayersTags.HIDER_LAYER)
+        if (other.gameObject.layer == _UIHideableLayer)
             other.gameObject.transform.GetChild(0).gameObject.SetActive(true);
     }
 }

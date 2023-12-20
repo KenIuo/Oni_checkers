32472e84afcecf3c8fbd6ef4a8010a64de55fca7
Adrian32 <amir-rahim@mail.ru>
2023-12-20 22:03:29 +0300

    удалены лишние файлы

--- Assets/Amir/Scripts/TagManager.cs
+++ Assets/Amir/Scripts/TagManager.cs
@@ -4,7 +4,7 @@ public class LayersTags
 {
     public static readonly LayerMask ARROW_LAYER = LayerMask.GetMask("UI_Arrows");
     public static readonly LayerMask PF_LAYER = LayerMask.GetMask("Playing_Field");
-    public static readonly LayerMask HIDER_LAYER = LayerMask.GetMask("UI_Hideable");
+    public static readonly LayerMask HIDER_LAYER = LayerMask.GetMask("UI_Hidable");
 }
 
 public class NamesTags
